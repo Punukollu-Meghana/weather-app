86fde98a6ab276838b2bb2f64dfd987ab146ba08
Punukollu-Meghana <meghana31p@gmail.com>
2025-10-06 11:43:36 +0530

    initial commit for weather app

---
+++ src/app/weather/weather.effects.ts
@@ -0,0 +1,41 @@
+import { HttpClient } from "@angular/common/http";
+import { inject, Injectable } from "@angular/core";
+import { Actions, createEffect, ofType } from "@ngrx/effects";
+import * as loadActions from './weather.actions';
+import { catchError, delay, map, switchMap } from "rxjs/operators";
+import { of } from "rxjs";
+
+@Injectable()
+export class Weathereffects {
+  private actions$ = inject(Actions);
+  private http = inject(HttpClient);
+
+
+// IMPORTANT !!!!!!!!!! Replcae the text 'REPLACE_IT_WITH_YOUR_ID' with your API KEY
+// ELSE APP WONT RUN   
+
+  loadWeather$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(loadActions.loadWeather),
+      switchMap(({city}) =>
+        this.http.get<any>(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=ebd83c1c4cdd4d646e94bb567d7bfb9b&units=metric`)
+        .pipe(
+          delay(1000)
+        )
+        .pipe(
+          map(res =>
+            loadActions.loadWeatherSuccess({
+              weather : {
+                city : res.name,
+                temperature : res.main.temp,
+                humidity : res.main.humidity,
+                description : res.weather[0].description
+              }
+            }),
+          ),
+          catchError(err => of(loadActions.loadWeatherFailure({error : err.message || 'Failed to load the message'})))
+        )
+      )
+    )
+  )
+}
---
+++ src/app/weather/weather.model.ts
@@ -0,0 +1,6 @@
+export interface Weather{
+    city:string;
+    temperature:number;
+    humidity:number;
+    description:string;
+} 
